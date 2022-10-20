43ea0e8f8eec68931af128bbb134e07b81923590
Mostafa amin <darshola0@gmail.com>
2022-10-21 00:30:08 +0200

    finish the project

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,14 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+// strat test 
+
+function convert() {
+    var amount = document.getElementById("dollar").value,
+        massege = document.getElementById("massege");
+
+    if (amount === "") {
+        massege.innerHTML = "The field must not be empty";
+    } else if (Number(amount)) {
+        massege.innerHTML = "The field must not contain numbers";
+    }
+}
